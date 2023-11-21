ff7321662cb22ccc1ccd20c44c8ffdb26f611026
Jittipat <Jittipat.ph@gmail.com>
2023-11-21 23:25:56 +0700

    remove redundant functions

--- fastapi/app/server/mqtt/sensor_data.py
+++ fastapi/app/server/mqtt/sensor_data.py
@@ -33,28 +33,13 @@ def connect(client, flags, rc, properties):
 async def message(client, topic, payload, qos, properties):
     print("Received message: ",topic, payload.decode(), qos, properties)
 
-@fast_mqtt.subscribe("/TGR_16")
+@fast_mqtt.on_subscribe("/TGR_16")
 async def message_to_topic(client, topic, payload, qos, properties):
     print("Received message to specific topic: ", topic, payload.decode(), qos, properties)
 
 @fast_mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
     print("Disconnected")
-
-@fast_mqtt.on_subscribe()
-def subscribe(client, mid, qos, properties):
-    print("subscribed", client, mid, qos, properties)
-
-
-# @router.get("/", response_description="test publish to mqtt")
-# async def publish_hello():
-#     fast_mqtt.publish("/mqtt", "Hello, world") #publishing mqtt topic
-#     return {"result": True,"message":"Published" }
-
-@fast_mqtt.on_connect()
-def connect(client, flags, rc, properties):
-    fast_mqtt.client.subscribe("/TGR_16") #subscribing mqtt topic
-    print("Connected: ", client, flags, rc, properties)
 
 @router.get("/", response_description="test publish to mqtt")
 async def publish_hello():
