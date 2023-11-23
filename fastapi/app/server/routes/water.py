6ca84f519e4fdde2af5b3864f199041eb807402d
Jittipat <Jittipat.ph@gmail.com>
2023-11-23 14:34:40 +0700

    comment out unused functions

--- fastapi/app/server/routes/water.py
+++ fastapi/app/server/routes/water.py
@@ -4,10 +4,10 @@ import requests, json
 
 from server.database import (
     add_water,
-    delete_water,
+    # delete_water,
     retrieve_water,
-    retrieve_waters,
-    update_water,
+    # retrieve_waters,
+    # update_water,
 )
 from server.models.water import (
     ErrorResponseModel,
@@ -24,12 +24,12 @@ async def add_water_data(water: WaterSchema = Body(...)):
     new_water = await add_water(water)
     return ResponseModel(new_water, "Water added successfully.")
 
-@router.get("/", response_description="Waters retrieved")
-async def get_waters():
-    waters = await retrieve_waters()
-    if waters:
-        return ResponseModel(waters, "Waters data retrieved successfully")
-    return ResponseModel(waters, "Empty list returned")
+# @router.get("/", response_description="Waters retrieved")
+# async def get_waters():
+#     waters = await retrieve_waters()
+#     if waters:
+#         return ResponseModel(waters, "Waters data retrieved successfully")
+#     return ResponseModel(waters, "Empty list returned")
 
 
 @router.get("/{id}", response_description="Water data retrieved")
@@ -39,32 +39,32 @@ async def get_water_data(id):
         return ResponseModel(water, "Water data retrieved successfully")
     return ErrorResponseModel("An error occurred.", 404, "Student doesn't exist.")
 
-@router.put("/{id}")
-async def update_water_data(id: str, req: UpdateWaterModel = Body(...)):
-    req = {k: v for k, v in req.dict().items() if v is not None}
-    updated_water = await update_water(id, req)
-    if updated_water:
-        return ResponseModel(
-            "Water with ID: {} name update is successful".format(id),
-            "Water name updated successfully",
-        )
-    return ErrorResponseModel(
-        "An error occurred",
-        404,
-        "There was an error updating the water data.",
-    )
+# @router.put("/{id}")
+# async def update_water_data(id: str, req: UpdateWaterModel = Body(...)):
+#     req = {k: v for k, v in req.dict().items() if v is not None}
+#     updated_water = await update_water(id, req)
+#     if updated_water:
+#         return ResponseModel(
+#             "Water with ID: {} name update is successful".format(id),
+#             "Water name updated successfully",
+#         )
+#     return ErrorResponseModel(
+#         "An error occurred",
+#         404,
+#         "There was an error updating the water data.",
+#     )
 
 
-@router.delete("/{id}", response_description="Water data deleted from the database")
-async def delete_water_data(id: str):
-    deleted_water = await delete_water(id)
-    if deleted_water:
-        return ResponseModel(
-            "Water with ID: {} removed".format(id), "Water deleted successfully"
-        )
-    return ErrorResponseModel(
-        "An error occurred", 404, "Water with id {0} doesn't exist".format(id)
-    )
+# @router.delete("/{id}", response_description="Water data deleted from the database")
+# async def delete_water_data(id: str):
+#     deleted_water = await delete_water(id)
+#     if deleted_water:
+#         return ResponseModel(
+#             "Water with ID: {} removed".format(id), "Water deleted successfully"
+#         )
+#     return ErrorResponseModel(
+#         "An error occurred", 404, "Water with id {0} doesn't exist".format(id)
+#     )
 
 @router.get("/get_then_post/{id}", response_description="water data retrieved")
 async def get_mockup_data(id):
